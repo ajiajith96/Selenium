2e079484af4beed244b125a2e0b3ad6eed79ae29
HP <HP@192.168.1.35>
2025-08-06 17:15:06 +0530

    Commit

---
+++ SeleniumProject/src/main/java/SeleniumBasics/Base.java
@@ -0,0 +1,21 @@
+package SeleniumBasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+	public WebDriver driver;
+	public void initializeBrowser() {
+		
+		driver = new ChromeDriver();
+		driver.get("https://www.obsqurazone.com/");
+		driver.manage().window().maximize();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Base base = new Base();
+        base.initializeBrowser();
+	}
+
+}
