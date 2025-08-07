1260c603e995cc82380b317cd42d276e505078db
HP <HP@192.168.1.35>
2025-08-07 15:39:32 +0530

    Commit

--- SeleniumProject/src/test/java/SeleniumBasic/Base.java
+++ SeleniumProject/src/test/java/SeleniumBasic/Base.java
@@ -1,4 +1,4 @@
-package SeleniumBasics;
+package SeleniumBasic;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
--- SeleniumProject/src/test/java/SeleniumBasic/BrowserCommands.java
+++ SeleniumProject/src/test/java/SeleniumBasic/BrowserCommands.java
@@ -1,4 +1,4 @@
-package SeleniumBasics;
+package SeleniumBasic;
 
 public class BrowserCommands extends Base {
 	
