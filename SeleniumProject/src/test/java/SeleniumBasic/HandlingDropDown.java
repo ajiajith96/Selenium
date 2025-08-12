64e655e59b3c02ec49b2b40532afa6886ca3a4ca
HP <HP@192.168.1.38>
2025-08-12 12:00:39 +0530

    Commit

--- SeleniumProject/src/test/java/SeleniumBasic/HandlingDropDown.java
+++ SeleniumProject/src/test/java/SeleniumBasic/HandlingDropDown.java
@@ -11,29 +11,41 @@ public class HandlingDropDown extends Base {
 		driver.navigate().to("https://www.webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
 		WebElement dropdown = driver.findElement(By.xpath("//select[@id='dropdowm-menu-1']"));
 		Select select = new Select(dropdown);
-		select.selectByIndex(3);
+		select.selectByIndex(0);
 		
-		select.selectByValue("sql");
+		//select.selectByValue("java");
 		
-		select.selectByVisibleText("JAVA");
+		//select.selectByVisibleText("JAVA");
 	
 	}
 	
 	public void handlingCheckBox() {
 		driver.navigate().to("https://www.webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
-		WebElement checkbox = driver.findElement(By.xpath("//input[@value='option-3']"));
-		checkbox.click();
-		System.out.println(checkbox.isSelected());
+		WebElement checkbox1 = driver.findElement(By.xpath("//input[@value='option-1']"));
+		checkbox1.click();
+		
+		WebElement checkbox2 = driver.findElement(By.xpath("//input[@value='option-2']"));
+		checkbox2.click();
+		System.out.println(checkbox1.isSelected());
+		System.out.println(checkbox2.isSelected());
+	}
+	
+	public void handlingRadioButton() {
+		driver.navigate().to("https://www.webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+		WebElement radiobutton = driver.findElement(By.xpath("//input[@value='yellow']"));
+		radiobutton.click();
+		System.out.println(radiobutton.isSelected());
 	}
 	
 	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		HandlingDropDown drop = new HandlingDropDown();
 		drop.initializeBrowser();
 		drop.handlingdropdown();
+		drop.handlingCheckBox();
+		drop.handlingRadioButton();
 		
 	}
 
