289288b81131b5c070a45764717ef0f29b293684
HP <HP@192.168.1.38>
2025-08-20 14:25:08 +0530

    Commit

---
+++ SeleniumProject/src/test/java/SeleniumBasic/JavaScriptExecuteSample.java
@@ -0,0 +1,26 @@
+package SeleniumBasic;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+public class JavaScriptExecuteSample extends Base{
+	
+	public void verifyExecute() {
+		
+		driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
+		WebElement showMessageButton=driver.findElement(By.xpath("//button[@id='button-one']"));
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].click();", showMessageButton);
+		js.executeScript("window.scrollBy(0,350)", "");  // for scrolling vertically or horizontally.(+ve value
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JavaScriptExecuteSample java = new JavaScriptExecuteSample();
+		java.initializeBrowser();
+		java.verifyExecute();
+
+	}
+
+}
